c4c4211d9c58fdb1d65b7096563a5f7aa1ec8fff
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-26 21:17:22 +0545

    Feat:Category,Tag and Blog model is created

--- app/models.py
+++ app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    author = models.CharField(max_length=100)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, related_name='blogs', on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='blogs')
