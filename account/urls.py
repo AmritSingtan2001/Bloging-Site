26505eeb511be02c896cf6f50696952d23c02897
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-26 20:55:44 +0545

    Refactor :Account urls.py file

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,8 @@ from account.views import  *
 
 
 urlpatterns = [
+    path('register', UserRegistrationView.as_view(), name='register'),
+    path('login', UserLoginView.as_view(), name='login'),
     path('profile', UserProfileView.as_view(), name='profile'),
     path('update/profile', UserProfileView.as_view(), name='profile'),
     path('change/password', UserChangePasswordView.as_view(), name='changepassword'),
