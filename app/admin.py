6a347af78a6fd651ab5acc3854f5405601bb905c
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-26 21:17:50 +0545

    Refactor :Admin.py file with registering models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Tag, Blog
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Category, CategoryAdmin)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+
+admin.site.register(Tag, TagAdmin)
+
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'creation_date', 'category_display']
+
+    def category_display(self, obj):
+        return obj.category.name
+    
+    category_display.short_description = 'Category'
+
+admin.site.register(Blog, BlogAdmin)
