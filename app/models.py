e751fa9a0f606c0303aab923d722a47e340b4563
AmritSingtan2001 <singtanamrit154@gmail.com>
2024-04-26 21:21:05 +0545

    Refactor:Blog Models

--- app/models.py
+++ app/models.py
@@ -1,15 +1,24 @@
 from django.db import models
 
 class Category(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(verbose_name='Category Name',max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Tag(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(verbose_name='Tag Name',max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
 
 class Blog(models.Model):
-    title = models.CharField(max_length=255)
-    content = models.TextField()
-    author = models.CharField(max_length=100)
+    title = models.CharField(verbose_name='Blog Title',max_length=255)
+    content = models.TextField(verbose_name='Descriptions ')
+    author = models.CharField(verbose_name='Author Name',max_length=100)
     creation_date = models.DateTimeField(auto_now_add=True)
-    category = models.ForeignKey(Category, related_name='blogs', on_delete=models.CASCADE)
-    tags = models.ManyToManyField(Tag, related_name='blogs')
+    category = models.ForeignKey(Category, related_name='blogs', verbose_name='Blog Category',on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag, related_name='blogs', verbose_name='Tag Name')
+
+    def __str__(self) -> str:
+        return self.title
